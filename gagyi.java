e0c1ff33e4ff210a7ed569cd0975b1dfa96abbc4
edeszilagyi <36382330+edeszilagyi@users.noreply.github.com>
2018-09-19 10:33:32 +0200

    Add files via upload

---
+++ gagyi.java
@@ -0,0 +1,26 @@
+﻿public class Gagyi {
+
+	public static void main(String[] args) {
+		
+		Integer i = 130;
+		Integer j = 130;
+		
+		if(i <= j && i >= j && i != j) {
+			System.out.println("1. Belép i = " + i + " j = " + j);
+		}	
+		
+		i = 10;
+		j = 10;
+		
+		if(i <= j && i >= j && i != j) {
+			System.out.println("2. Belép i = " + i + " j = " + j);
+		}
+		
+		i = new Integer(10);
+		j = new Integer(10);
+		
+		if(i <= j && i >= j && i != j) {
+			System.out.println("3. Belép i = " + i + " j = " + j);
+		}
+	}
+}
