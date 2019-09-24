90353eb0b7fcd9382ab5e107d5e815e54ecdf62f
edeszilagyi <ede.szilagyi@yahoo.com>
2019-09-24 22:52:01 +0200

    update

--- 1.Het/Gagyi.java
+++ 1.Het/Gagyi.java
@@ -5,6 +5,10 @@ public class Gagyi {
 		Integer i = 10;
 		Integer j = 10;
 		
+		/*vegtelen ciklusos valtozok:
+		Integer i = 130;
+		Integer j = 130;*/
+		
 		System.out.println("i = " + i + " j = " + j);
 
 		while(i <= j && i >= j && i != j) {
